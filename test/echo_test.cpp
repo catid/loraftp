266ff0486e76ee007b94e8b1a668ce4f99a0a8da
Christopher A. Taylor <mrcatid@gmail.com>
2020-09-17 15:33:31 -0700

    wip

--- test/echo_test.cpp
+++ test/echo_test.cpp
@@ -60,34 +60,39 @@ int main(int argc, char* argv[])
 
         const int packet_bytes = 200;
 
-        uint64_t t1 = GetTimeMsec();
-        int64_t dt = t1 - t0;
-        if (id != -1 && dt > send_interval_msec && waveshare.GetSendQueueBytes() == 0) {
-            uint8_t data[packet_bytes] = {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-            };
-            *(uint32_t*)data = counter++;
-            if (!waveshare.Send(data, packet_bytes)) {
-                cerr << "waveshare.Send failed" << endl;
-                return -1;
+        if (id != -1)
+        {
+            uint64_t t1 = GetTimeMsec();
+            int64_t dt = t1 - t0;
+            if (dt > send_interval_msec && waveshare.GetSendQueueBytes() == 0) {
+                uint8_t data[packet_bytes] = {
+                    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+                };
+                *(uint32_t*)data = counter++;
+                if (!waveshare.Send(data, packet_bytes)) {
+                    cerr << "waveshare.Send failed" << endl;
+                    return -1;
+                }
+                cout << "Sent: Ping SendQueueBytes=" << waveshare.GetSendQueueBytes() << endl;
+                t0 = t1;
             }
-            cout << "Sent: Ping SendQueueBytes=" << waveshare.GetSendQueueBytes() << endl;
-            t0 = t1;
         }
-
-        uint8_t buffer[packet_bytes];
-        int bytes = waveshare.Receive(buffer, packet_bytes, packet_bytes);
-        if (bytes < 0) {
-            cerr << "Link broken" << endl;
-            break;
-        }
-        if (bytes > 0) {
-            cout << "Got bytes:";
-            for (int i = 0; i < bytes; ++i) {
-                cout << " " << (int)buffer[i];
+        else
+        {
+            uint8_t buffer[packet_bytes];
+            int bytes = waveshare.Receive(buffer, packet_bytes, packet_bytes);
+            if (bytes < 0) {
+                cerr << "Link broken" << endl;
+                break;
             }
-            //cout << " at RSSI = " << buffer[bytes - 1] * 0.5f << " dBm" << endl;
-            cout << endl;
+            if (bytes > 0) {
+                cout << "Got bytes:";
+                for (int i = 0; i < bytes; ++i) {
+                    cout << " " << (int)buffer[i];
+                }
+                //cout << " at RSSI = " << buffer[bytes - 1] * 0.5f << " dBm" << endl;
+                cout << endl;
+            }
         }
     }
 
